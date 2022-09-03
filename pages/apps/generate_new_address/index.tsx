e4a6146726879db01b8c9e5b664391c25dcd1f25
xijieyin <xijieyin@gmail.com>
2022-09-03 20:36:55 +0800

    feat: first web3 tool app

--- pages/apps/generate_new_address/index.tsx
+++ pages/apps/generate_new_address/index.tsx
@@ -3,16 +3,32 @@ import { NextPage } from "next";
 import AppStep from "../../../components/AppStep";
 import Button from "components/buttons/Button";
 import AppTitle from "blocks/apps/AppTitle";
+import { EvmAddress, generateAddress } from "./utils";
+import { useState } from "react";
 
 const Index: NextPage = () => {
   const meta = {};
+  const [address, setAddresss] = useState<EvmAddress | undefined>(undefined);
+
   return (
     <Layout meta={meta}>
       <div className="flex flex-col gap-4">
         <AppTitle name="Generate new address"></AppTitle>
         <AppStep step={1}>
           <div>
-            <Button className="bg-solana">Generate</Button>
+            <Button
+              onClick={() => setAddresss(generateAddress())}
+              className="bg-solana mb-4"
+            >
+              Generate
+            </Button>
+            {address && (
+              <ul className="text-xl">
+                <li>Address: {address.address}</li>
+                <li>Mnemonic: {address.mnemonic}</li>
+                <li>Private Key: {address.privateKey}</li>
+              </ul>
+            )}
           </div>
         </AppStep>
       </div>
---
+++ pages/apps/generate_new_address/utils.ts
@@ -0,0 +1,15 @@
+import * as ethers from "ethers";
+
+export type EvmAddress = {
+  address: string;
+  privateKey: string;
+  mnemonic: string;
+};
+export function generateAddress(): EvmAddress {
+  const wallet = ethers.Wallet.createRandom();
+  return {
+    address: wallet.address,
+    privateKey: wallet.privateKey,
+    mnemonic: wallet.mnemonic.phrase,
+  };
+}
