f7fcb248d9429def002d831da3a97c437786ebaa
stonegate <stonega@users.noreply.github.com>
2023-05-21 17:08:29 +0800

    fix: minor update

--- blocks/layout/NavBar.tsx
+++ blocks/layout/NavBar.tsx
@@ -7,7 +7,7 @@ const NavBar = () => {
     <div className="max-w-6xl w-full my-4 px-4 flex justify-between items-center">
       <Link href="/">
         <span className="text-2xl text-black dark:text-white px-4 md:px-0 md:text-4xl font-medium flex items-center">
-          Web3.0{" "}
+          Web3{" "}
           <span className="border-2 ml-2 px-2 rounded-lg border-black dark:border-white text-2xl">
             Helpers
           </span>
